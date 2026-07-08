7df8b2a483bf55a779e7b723a9f19d9c8df6e0d6
Gouri-292 <gouriagrawal06@gmail.com>
2026-07-08 19:53:59 +0530

    Migrated to PostgreSQL and updated frontend URLs

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://smart-health-sbqq.onrender.com',
         changeOrigin: true,
         secure: false,
       },
